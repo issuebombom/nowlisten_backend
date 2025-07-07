ac79db1b7ea11c1d5959088156c4cc552c6880ab
issuebombom <issuebombom@gmail.com>
2025-07-07 12:05:34 +0900

    fix: postgre setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (ConfigService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: 
-
-      })
-    })
+        host: configService.get<string>('DB_HOST'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true, // 개발 환경에서만 true로 설정
+        logging: true, // 개발 환경에서만 true로 설정
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
